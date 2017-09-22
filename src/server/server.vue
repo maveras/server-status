642c1bcec937d9dd15e9c5938aaf597b5288621d
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2017-09-22 11:24:09 -0300

    created a eventBus and data to manage the server status

--- src/server/server.vue
+++ src/server/server.vue
@@ -3,26 +3,35 @@
     <li
     style="cursor: pointer"
     class="list-group-item"
+    @click="serverDetail(server)"
     v-for="server in servers">
     Server #{{ server.id }}
   </li>
 </div>
 </template>
 <script>
+import {eventBus} from '../main'
+
 export default {
   data () {
     return {
-      servers: [
+      servers:[
         {
-          id:1 , status:'online',
+          id:1 , status:'online'
         },
         {
-          id:2 , status:'offline',
+          id:2 , status:'offline'
         },
         {
-          id:3 , status:'unknow',
+          id:3 , status:'unknow'
         }
       ]
+    }
+  },
+  methods: {
+    serverDetail (server) {
+      console.log('¡asdasdas')
+      eventBus.$emit('changeServer',server)
     }
   }
 }
--- src/server/serverDetail.vue
+++ src/server/serverDetail.vue
@@ -1,5 +1,25 @@
 <template>
 	<div class="col-xs-12 col-sm-6">
-    <p>Server Details are currently not updated</p>
+    <p v-if='server==null'>Server Details are currently not updated</p>
+    <p v-else> The server status of the server {{server.id}} is {{server.status}}</p>
   </div>
 </template>
+
+<script>
+import {eventBus} from '../main'
+
+export default {
+  data () {
+    return {
+      server: null
+    }
+  },
+  created () {
+   
+    eventBus.$on('changeServer', (server) => {
+      this.server = server
+       console.log('oaseee')
+    })
+  } 
+}
+</script>
